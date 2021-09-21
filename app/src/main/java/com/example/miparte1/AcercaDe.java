82cee24bc563282c92f34d53d845f45d6c47b977
BrianAnco <ancobrian6@gmail.com>
2021-09-21 12:23:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/miparte1/AcercaDe.java
@@ -0,0 +1,20 @@
+package com.example.miparte1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class AcercaDe extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+
+}
